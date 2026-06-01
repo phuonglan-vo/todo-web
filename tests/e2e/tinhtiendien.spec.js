98273c157553d5e8343a61282bd6f82f67f6d3a5
vplan <phuonglan060295@gmail.com>
2026-06-01 21:37:45 +0700

    bổ sung verify màu

--- tests/e2e/tinhtiendien.spec.js
+++ tests/e2e/tinhtiendien.spec.js
@@ -11,15 +11,23 @@ test.describe("Kiểm thử chức năng tính tiền điện", () => {
         //Thao tác click vào nút có id "calculate-button"
         await page.click("#calculate-btn");
         //Lấy phần tử vùng thông báo id calculation-message
-        const loginMessage = page.locator('#calculation-message');
+        const tinhtienMessage = page.locator('#calculation-message');
         //Mong đợi kết quả trong vùng thông báo phải là "Vui lòng nhập số kwh"
-        await expect(loginMessage).toHaveText("Vui lòng nhập số kwh");  
+        await expect(tinhtienMessage).toHaveText("Vui lòng nhập số kwh");  
     });
     //Test case 2: nhập số kwh là 0, vùng thông báo phải hiển thị "Số tiền phải trả là 0 VNĐ"
     test("TC002 - Hiển thị thông báo lỗi khi nhập số kwh là 0", async ({ page }) => {
         await page.fill("#kwh-input", "0");
         await page.click("#calculate-btn");
-        const loginMessage = page.locator('#calculation-message');
-        await expect(loginMessage).toHaveText("Số tiền điện phải trả là 0 VNĐ");
+        const tinhtienMessage = page.locator('#calculation-message');
+        await expect(tinhtienMessage).toHaveText("Số tiền điện phải trả là 0 VNĐ");
+    });
+    //Test case 3: số tiền điện phải trả dưới 20.000 VNĐ, vùng thông báo phải hiển thị màu đỏ
+    test("TC003 - Hiển thị thông báo màu đỏ khi số tiền điện phải trả dưới 20.000 VNĐ", async ({ page }) => {
+        await page.fill("#kwh-input", "10");
+        await page.click("#calculate-btn");
+        const tinhtienMessage = page.locator('#calculation-message span');
+        await expect(tinhtienMessage).toHaveText("Số tiền điện phải trả là 18000 VNĐ");
+        await expect(tinhtienMessage).toHaveCSS("color", "rgb(255, 0, 0)"); //Màu đỏ
     });
 });
