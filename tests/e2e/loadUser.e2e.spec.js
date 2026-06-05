075dc9acea874baddb3c535efefb849dd61642db
vplan <phuonglan060295@gmail.com>
2026-06-05 21:23:44 +0700

    bổ sung case TC02 của e2e gọi API

--- tests/e2e/loadUser.e2e.spec.js
+++ tests/e2e/loadUser.e2e.spec.js
@@ -9,3 +9,14 @@ test("TC-E2E-01: nhập URL và hiển thị danh sách UL LI", async ({ page })
     await expect(page.locator('#result ul')).toBeVisible();
     await expect(page.locator('#result li')).toHaveCount(10);
 });
+test("TC-E2E-02: kiểm tra dữ liệu đúng", async({page}) => {
+    await page.goto("/danh-sach-users.html");
+
+    await page.locator('#api-url');
+    await page.click('#fetch-button');
+
+    const result = page.locator('#result');
+    await expect(result).toContainText('Ervin Howell');
+    await expect(result).toContainText('Kurtis Weissnat');
+    await expect(result).toContainText('Glenna Reichert');
+});
